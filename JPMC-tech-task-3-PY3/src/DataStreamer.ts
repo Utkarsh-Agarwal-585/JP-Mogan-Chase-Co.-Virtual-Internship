5205812d92eb6f05d9f56a768954c6e34eb7d6d3
Utkarsh Agarwal <agrawalu01@gmail.com>
2020-12-16 22:45:49 +0530

    Attached Patch File

---
+++ JPMC-tech-task-3-PY3/src/DataStreamer.ts
@@ -0,0 +1,40 @@
+export interface Order {
+  price: number,
+  size: number,
+}
+export interface Row{
+  price_abc: number,
+  price_def:number,
+  ratio:number,
+  timestamp: Date,
+  upper_bound:number,
+  lower_bound:number,
+  trigger_alert: number | undefined,
+}
+export interface ServerRespond {
+  stock: string,
+  top_bid: Order,
+  top_ask: Order,
+  timestamp: Date,
+}
+
+class DataStreamer {
+  static API_URL: string = 'http://localhost:8080/query?id=1';
+
+  static getData(callback: (data: ServerRespond[]) => void): void {
+    const request = new XMLHttpRequest();
+    request.open('GET', DataStreamer.API_URL, false);
+
+    request.onload = () => {
+      if (request.status === 200) {
+        callback(JSON.parse(request.responseText));
+      } else {
+        alert ('Request failed');
+      }
+    }
+
+    request.send();
+  }
+}
+
+export default DataStreamer;
---
+++ README.md
@@ -0,0 +1,11 @@
+# JPMorgan Chase & Co. Virtual Internship
+
+### This repository contains the submitted patch files of the various tasks assigned by JPMorgan Chase & Co. Software Engineering Virtual Internship.
+
+## Website Link :
+
+[https://www.insidesherpa.com/virtual-internships/R5iK7HMxJGBgaSbvk](https://www.insidesherpa.com/virtual-internships/R5iK7HMxJGBgaSbvk)
+
+## For any queries/doubts:
+
+:envelope: taurusu222@gmail.com :thumbsup:
